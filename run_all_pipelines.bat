624aa8f55fe0969e485d3850cac54c2d85af0566
panxiaoyan225-sudo <panxiaoyan225@gmail.com>
2026-02-01 19:21:53 -0500

    update

--- run_all_pipelines.bat
+++ run_all_pipelines.bat
@@ -4,6 +4,7 @@ cd /d "C:\Python\pipeline"
 
 :: 2. Activate the virtual environment from the NEW location
 call "C:\Python\venv\Scripts\activate"
+#call ..\venv\Scripts\activate
 
 :: 3. Run scripts from the NEW /dags subfolder
 python .\pipeline_py\titanic_pipeline.py
